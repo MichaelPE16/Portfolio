7cba29a62f2dcfe596bd698b10a2a0ebcec4bab8
michaelPE16 <rafael112216@gmail.com>
2026-01-17 08:19:40 -0500

    Fix Read-only file system error by removing os.makedirs

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -127,8 +127,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-os.makedirs(STATIC_ROOT, exist_ok=True)
+# Vercel filesystem is read-only, use /tmp for static files if needed
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+if not os.path.exists(STATIC_ROOT):
+     STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
