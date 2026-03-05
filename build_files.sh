5b431d71ae141206b6cb41868e1c9059d5655ac6
michaelPE16 <rafael112216@gmail.com>
2026-03-05 16:53:42 -0500

    Adding the fixed on buildingfiles 1.4

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 echo "BUILD START"
 mkdir -p staticfiles_build
-curl -sS https://bootstrap.pypa.io/get-pip.py | python3.9
-python3.9 -m pip install -r requirements.txt --break-system-packages
-python3.9 manage.py collectstatic --noinput --clear
+
+if command -v python3.12 &>/dev/null; then
+    PYTHON_CMD=python3.12
+elif command -v python3.11 &>/dev/null; then
+    PYTHON_CMD=python3.11
+elif command -v python3.10 &>/dev/null; then
+    PYTHON_CMD=python3.10
+else
+    PYTHON_CMD=python3
+fi
+
+echo "Using Python executable: $PYTHON_CMD"
+$PYTHON_CMD --version
+
+curl -sS https://bootstrap.pypa.io/get-pip.py | $PYTHON_CMD
+$PYTHON_CMD -m pip install -r requirements.txt --break-system-packages
+$PYTHON_CMD manage.py collectstatic --noinput --clear
 echo "BUILD END"
