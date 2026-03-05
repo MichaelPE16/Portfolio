6b36097c420b05226b58fdef5b05b49ec8508469
michaelPE16 <rafael112216@gmail.com>
2026-03-05 16:43:53 -0500

    Adding the fixed on buildingfiles 1.2

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "BUILD START"
-python3.9 -m pip install -r requirements.txt --break-system-packages
-python3.9 manage.py collectstatic --noinput --clear
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python manage.py collectstatic --noinput --clear
 echo "BUILD END"
